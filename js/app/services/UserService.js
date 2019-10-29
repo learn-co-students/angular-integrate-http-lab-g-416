e60c70ab1444b9cbd3f9db0c92189543c8e6442e
daschne8 <daschne8@gmail.com>
2019-10-29 14:29:58 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,14 @@
 function UserService($http) {
 
+	this.user = ''
+
+	this.getUser = function(){
+		return $http.get('http://0.0.0.0:8882/rest/user')
+	}
+
+	this.updateUser = function(){
+		return $http.post('http://0.0.0.0:8882/rest/user', {name: 'Derp'})
+	}
 }
 
 angular
